83c6ec63899685ef4049788ae0d353f31010ece2
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-02-10 12:08:38 +0100

    Modified base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Defines a BaseModel """
-import uuid
+from uuid import uuid4
 from datetime import datetime
 import models
 
@@ -24,7 +24,7 @@ class BaseModel:
                         v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
                     setattr(self, k, v)
         else:
-            self.id = str(uuid.uuid4())
+            self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
             models.storage.new(self)
