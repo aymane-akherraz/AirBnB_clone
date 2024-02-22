d1654366698dd80a055bdcabd55d136a3dc0ba5b
Aymane Akherraz <aymaneakherraz@gmail.com>
2024-02-22 17:56:25 +0100

    Modified test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -78,6 +78,33 @@ class TestBasemodel(unittest.TestCase):
         self.b.save()
         self.assertGreater(self.b.updated_at, old_dt)
 
+    
+    def test_one_save(self):
+        """Test for save method"""
+        bm = BaseModel()
+        sleep(0.05)
+        first_updated_at = bm.updated_at
+        bm.save()
+        self.assertLess(first_updated_at, bm.updated_at)
+
+    def test_two_saves(self):
+        """Test for save method"""
+        bm = BaseModel()
+        sleep(0.05)
+        first_updated_at = bm.updated_at
+        bm.save()
+        second_updated_at = bm.updated_at
+        self.assertLess(first_updated_at, second_updated_at)
+        sleep(0.05)
+        bm.save()
+        self.assertLess(second_updated_at, bm.updated_at)
+
+    def test_save_with_arg(self):
+        """Test for save method"""
+        bm = BaseModel()
+        with self.assertRaises(TypeError):
+            bm.save(None)
+
 
 if __name__ == '__main__':
     unittest.main()
